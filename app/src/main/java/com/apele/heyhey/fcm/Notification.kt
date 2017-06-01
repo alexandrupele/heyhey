e689423a72f3d31d1e8f4c5a710c90f3f906dca4
Alexandru Pele <alexandru.pele@fortech.ro>
2017-06-01 15:04:19 +0200

    update app icon

--- app/src/main/java/com/apele/heyhey/fcm/Notification.kt
+++
@@ -1,8 +0,0 @@
-package com.apele.heyhey.fcm
-
-/**
- * Created by alexandrupele on 31/05/2017.
- */
-data class Notification(val title: String,
-                        val text: String,
-                        val to: String)
---
+++ app/src/main/java/com/apele/heyhey/fcm/NotificationPayload.kt
@@ -0,0 +1,6 @@
+package com.apele.heyhey.fcm
+
+/**
+ * Created by alexandrupele on 31/05/2017.
+ */
+data class Notification(val title: String, val body: String)
